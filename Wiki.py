168004656e95c8bf4400a96edbc12fcdef4aa227
Jacob Sosa <jacobososa08@gmail.com>
2024-10-08 19:00:11 +0000

    Wiki day 2

--- Wiki.py
+++ Wiki.py
@@ -1,5 +1,6 @@
 import wikipediaapi
 import time
+from queue import Queue
 
 userAgent = "JS---Wiki (jacobososa08@gmail.com)"
 wiki = wikipediaapi.Wikipedia(userAgent, "en")
@@ -13,8 +14,43 @@ def fetchLinks(page):
 
     return linksList
 
+def wikipediaSolver(startPage, targetPage):
+    print("Working on it...")
+    startTime = time.time()
+
+    visited = set()
+    queue = Queue()
+    parent = {}
+
+    while not queue.empty():
+        currentPageTitle = queue.get()
+        if currentPageTitle == targetPage.title:
+            break
+
+        currentPage = wiki.page(currentPageTitle)
+        links = fetchLinks(currentPage)
+
+        for link in links:
+            if(link not in visited):
+                queue.put(link)
+                visited.add(link)
+                parent[link] = currentPageTitle
+
+    path = []
+    pageTitle = targetPage.title
+    while pageTitle != startPage.title:
+        path.append(pageTitle)
+        pageTitle = parent[pageTitle]
+    path.append(startPage.title)
+    path.reverse()
+
+    endTime = time.time
+    print("This took", endTime - startTime, "seconds")
+    return
+
 #creating start and target pages
 startPage = wiki.page("Puerto Rico")
-targetPage = wiki.page("Christianity")
-
+targetPage = wiki.page("Barack Obama")
+path = wikipediaSolver(startPage, targetPage)
 print(fetchLinks(startPage))
+
